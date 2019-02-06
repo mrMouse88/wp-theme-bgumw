4118f5cc1e10a96e736e54f8b88e81966de11673
MrMouse <it@asygnata.com>
2019-02-07 00:44:16 +0100

    added footer menu

--- footer.php
+++ footer.php
@@ -1,5 +1,8 @@
 <?php wp_footer(); ?>
 <footer class="bg-footer">
+    <div id="bg-footer-menu">
+        <?php wp_nav_menu(array('theme_location' => 'footer-menu')) ?>
+    </div>
     copyright
 </footer>
 
