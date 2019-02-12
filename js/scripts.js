e8bb8de410dafd61e33e79a8ae321e4a8cdfe287
MrMouse <it@asygnata.com>
2019-02-13 00:47:04 +0100

    faq card style

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,24 @@
-$( document ).ready(function() {
-    var windowHeight = $( window ).height();
-    var bodyHeight = $( "body" ).height();  
-    if(bodyHeight < windowHeight){
+function checkHeight() {
+    var windowHeight = $(window).height();
+    var bodyHeight = $("body").height();
+    if (bodyHeight < windowHeight) {
         $(".bg-footer").css('bottom', 0);
+    } else {
+        $(".bg-footer").css('bottom', 'auto');
     }
+}
+
+//sticky footer
+$(document).ready(function () {
+    checkHeight();
+});
+
+//folding faq cards
+$(document).ready(function () {
+    $(".bg-faq-title").click(function () {
+        $(this).parent().children(".bg-faq-content").slideToggle(function () {
+            $(this).parent().children().children(".bg-faq-icon").toggleClass("up down");
+            checkHeight();
+        });
+    });
 });
