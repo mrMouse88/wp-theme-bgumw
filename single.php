a889528994d72b0f31b7fafabe77bf51aadd955b
MrMouse <it@asygnata.com>
2019-02-13 23:54:02 +0100

    category uslugi page

--- single.php
+++ single.php
@@ -9,7 +9,9 @@ the_post_thumbnail('thumbnail');
     the_title();
 ?>
     </h1>
+    <?php if ( !in_category( 'uslugi' ) ) : ?>
     <span class="bg-post-date"><?php echo "(Dodano: ".get_the_date( 'd.m.Y' ).")"; ?></span>
+    <?php endif; ?>
     <?php if(have_posts()) : while(have_posts()) : the_post();?>
         
         <?php the_content();?>
